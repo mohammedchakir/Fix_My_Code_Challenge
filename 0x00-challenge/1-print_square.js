9760cd9258529703adf414bb825750cec3b4cac3
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-12-15 11:40:54 +0100

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 /*
     Print a square with the character #
     
@@ -7,23 +6,19 @@
     of the program.
 */
 
-// Check if the number of command-line arguments is less than 2
+
 if (process.argv.length <= 2) {
-    // Print an error message to the standard error
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./print_square.js <size>\n");
     process.stderr.write("Example: ./print_square.js 8\n");
-    process.exit(1); // Exit the program with an error code
+    process.exit(1);
 }
 
-// Parse the size argument from the command line as an integer
 const size = parseInt(process.argv[2]);
 
-// Loop to print the square
 for (let i = 0; i < size; i++) {
-    // Loop to print each row of the square
     for (let j = 0; j < size; j++) {
-        process.stdout.write("#"); // Print a #
+        process.stdout.write("#");
     }
-    process.stdout.write("\n"); // Move to the next line after each row
+    process.stdout.write("\n");
 }
